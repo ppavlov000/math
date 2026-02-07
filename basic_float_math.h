b241d0865dbf8dbbddae8efca1e6f9246bfd8254
Petro Pavlov <ppavlov000@gmail.com>
2026-02-07 19:25:54 +0100

    Minor changes

--- basic_float_math.h
+++ basic_float_math.h
@@ -3,14 +3,14 @@
 
 #include "basic_types.h"
 
-INLINE void deinterleave(float2_t* in, float_t* out0, float_t* out1, intg_t numSamples) {
+INLINE void Deinterleave(float2_t* in, float_t* out0, float_t* out1, intg_t numSamples) {
    for (intg_t i = 0; i < numSamples; i++) {
         out0[i] = in[i].a;
         out1[i] = in[i].b;
    }
 }
 
-INLINE void interleave(float_t* in0, float_t* in1, float2_t* out, intg_t numSamples) {
+INLINE void Interleave(float_t* in0, float_t* in1, float2_t* out, intg_t numSamples) {
     for (intg_t i = 0; i < numSamples; i++) {
         out[i].a = in0[i];
         out[i].b = in1[i];
