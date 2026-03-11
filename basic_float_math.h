1420bb957d8ec80670a7b7c5501d340f2b35e8d5
Petro Pavlov <ppavlov000@gmail.com>
2026-03-11 11:48:46 +0100

    [Math] Add float point helpers

--- basic_float_math.h
+++ basic_float_math.h
@@ -3,6 +3,9 @@
 
 #include "basic_types.h"
 
+#define LIN_TO_DB(x)   (20.f * log10f((x) + COMP_LOG_FLOOR))
+#define DB_TO_LIN(x)   powf(10.f, (x) * 0.05f)
+
 INLINE void Deinterleave(float2_t* in, float_t* out0, float_t* out1, intg_t numSamples) {
    for (intg_t i = 0; i < numSamples; i++) {
         out0[i] = in[i].a;
